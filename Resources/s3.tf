3409a41a943ace6373b24982783cd37a3298c9fe
rapakakarthik <rapakakarthik7@gmail.com>
2024-07-23 18:07:44 +0530

    added s3 resources

--- Resources/s3.tf
+++ Resources/s3.tf
@@ -1,14 +1,14 @@
-# #########################################Creating S3 bucket###################################################
+#########################################Creating S3 bucket###################################################
 
-# resource "aws_s3_bucket" "main" {
-#   bucket = var.mybucket.id
-# }
+resource "aws_s3_bucket" "main" {
+  bucket = var.mybucket
+}
 
-# resource "aws_s3_bucket_versioning" "main" {
-#     bucket = var.mybucket.vpc.id
-#     versioning_configuration {
-#       status = "Enabled"
-#     }
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = var.mybucket
+  versioning_configuration {
+    status = "Enabled"
+  }
 
-# }
+}
 
