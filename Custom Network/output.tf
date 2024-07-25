1930e84844681a8453cb78985c3de466e39f4247
Karthik <130748707+rapakakarthik@users.noreply.github.com>
2024-07-26 00:58:41 +0530

    Update output.tf

--- Custom Network/output.tf
+++ Custom Network/output.tf
@@ -15,22 +15,3 @@ output "instance_arn" {
 
 }
 
-*************************************************************************************************************************
-
-### Using `terraform output` Commands
-
-**Show All Outputs:**
-
-- To display all defined output variables after applying the configuration, use:
-
-`terraform output`
-
-**Show a Specific Output:**
-
-- To display the value of a specific output variable, use:
-
-`terraform output <output_name>`
-
-For example, to display the instance_public_ip:
-
-`terraform output instance_public_ip`
