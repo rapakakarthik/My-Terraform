6091aa01b68aedbf8a3f9b3502d1bd81e036b2e0
rapakakarthik <rapakakarthik7@gmail.com>
2024-07-31 17:07:07 +0530

    Terraform-script-jenkins

---
+++ Terraform-script-Jenkins/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-2"   #ohio
+}
+
+
+resource "aws_instance" "git" {
+    ami = "ami-00db8dadb36c9815e"
+    instance_type = "t2.micro"
+    key_name = "docker"
+
+
+    tags = "my-git-terraform-jenkins"
+  
+}
